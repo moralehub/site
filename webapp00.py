a4ecb1c2fdf6a1740ddcc98cc23f75fecba078a6
moralehub <otaviomorale12@gmail.com>
2024-11-13 21:57:43 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -8,10 +8,10 @@ st.title("UniConstruction | Requisições")
 st.header("Materiais")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("")
+st.subheader("Solicitações")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("")
+st.write("Usuarios")
 
 import streamlit as st
 
@@ -20,7 +20,3 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.header("Bloco estrutural 14 x 19 x 29cm")
     st.image("https://res.cloudinary.com/glide/image/fetch/f_auto,w_500,c_limit/https%3A%2F%2Fpavibloco.com.br%2Fwp-content%2Fuploads%2F2018%2F01%2FF29-L14-Bloco-Desenho-t%25C3%25A9cnico-1.jpg")
-
-with col2:
-    st.header("carro da lenda")
-    st.image(""
